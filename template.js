07abe397284c06d6ec39b054bb5f3903c224a7b2
ckc2007 <ckc2007@gmail.com>
2023-04-24 12:34:11 -0400

    adding js template

---
+++ template.js
@@ -0,0 +1,42 @@
+let solution = `
+# ${title}
+
+${tagline}.
+
+## Description
+
+${what}. ${why}. ${how}. ${learned}.
+
+${tableOfContents}
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+\```md
+![${imgAltText}](assets/images/screenshot.png)
+\```
+
+## Credits
+
+${collaboratorName}: ${collaboratorGitHubURL}
+${thirdPartyAssets}
+${tutorialLink}
+
+## License
+
+${license}
+
+## Features
+
+${features}
+
+## Tests
+
+${tests}
+
+`;
