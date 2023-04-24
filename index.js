63b6e872812513c369742190b5b654a38d28a49d
ckc2007 <ckc2007@gmail.com>
2023-04-24 10:50:52 -0400

    added template require

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+// require my template file
+const template = require("./template");
 
 // TODO: Create an array of questions for user input
 const questions = [];
